12307292381e141b8caf1b3c8e5705683480436a
FrancisRohith <166921745+FrancisRohith@users.noreply.github.com>
2025-07-06 14:40:13 +0530

    Create loop_led.s

---
+++ GPIO_LED/loop_led.s
@@ -0,0 +1,76 @@
+;------------------------------------------------------------------------------------------------------
+; A Simple SoC  Application
+; Toggle LEDs at a given frequency. 
+;------------------------------------------------------------------------------------------------------
+
+
+
+; Vector Table Mapped to Address 0 at Reset
+
+						PRESERVE8
+                		THUMB
+
+        				AREA	RESET, DATA, READONLY	  			; First 32 WORDS is VECTOR TABLE
+        				EXPORT 	__Vectors
+					
+__Vectors		    	DCD		0x00003FFC							; 16K Internal Memory
+        				DCD		Reset_Handler
+        				DCD		0  			
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD 	0
+        				DCD		0
+        				DCD		0
+        				DCD 	0
+        				DCD		0
+        				
+        				; External Interrupts
+						        				
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+              
+                AREA |.text|, CODE, READONLY
+;Reset Handler
+Reset_Handler   PROC
+                GLOBAL Reset_Handler
+                ENTRY
+
+AGAIN		   	LDR 	R1, =0x50000000				;Write to LED with value 0x55
+				    LDR		R0, =0xFF
+
+
+Loop				STR		R0, [R1]					
+				    LDR		R2, =0x20				;Delay
+
+
+Delay			SUBS	R2,R2,#1
+    			BNE 	Delay
+    			SUBS	R0, R0, #1
+    			BPL     Loop
+    			B       AGAIN
+
+			
+  				ALIGN 		4						; Align to a word boundary
+
+		      END                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+   
